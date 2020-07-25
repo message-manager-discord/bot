2654d77a4ad0313aebef157cb16230d3eebb74a2
Another Cat <keesam20@gmail.com>
2020-07-25 18:59:44 +0800

    Add send and edit commands, Add help for them.

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
+# helpers.py
 from discord import Embed
 def create_embed(title_value, colour_value, values):
     new_embed = Embed(title=title_value, colour = colour_value)
     for a in values:
         new_embed.add_field(name=a[0],value=a[1],inline=a[2])
     return new_embed
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import discord
 import platform
 import datetime
-import embeds
+import helpers
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -27,13 +27,24 @@ async def on_ready():
 
 @bot.command(name='help', help='Responds with an embed with all the commands and options')
 async def help(ctx):
-    embed=embeds.create_embed(
+    embed=helpers.create_embed(
     "Help with commands for the bot",
     16761035,
      [
-         [f"{prefix}ping", "Replys with the latency of the bot", True],
-         [f"{prefix}help", "Displays this view.", True],
-         [f'{prefix}info', 'Displays info about the bot', True],
+         [f"`{prefix}ping`", "Replys with the latency of the bot", True],
+         [f"`{prefix}help`", "Displays this view.", True],
+         [f'`{prefix}info`', 'Displays info about the bot', True],
+         [
+             f'`{prefix}edit channel_id message_id new_content`',
+             'Edits a message, message **must** be from the bot for it to work',
+             True
+         ],
+        [
+            f"`{prefix}send channel_id content`",
+            "Sends a message from the bot in the specificed channel",
+            True
+        ]
+
      ]
 )    
     await ctx.send(embed=embed)
@@ -41,21 +52,63 @@ async def help(ctx):
 
 @bot.command(name = 'info')
 async def info(ctx):
-    embed=embeds.create_embed(
-    "Info about the Bot",
-    discord.Colour(0xc387c1),
-     [
+    embed_content = [
          ["Username", bot.user, True],
          ["Prefix", prefix, True],
-         ["Developer",'<@684964314234618044>', , True],
-         [f'{prefix}info', 'Displays info about the bot', True],
-         ["Owner", f"<@{OWNER}>", True],
+         ["Developer",'<@684964314234618044>', True],
          ["Discord.py Version", platform.python_version(), True],
          ["Number of Servers",len(bot.guilds), True]
      ]
+    if OWNER != 'None':
+        embed_content.insert(3,["Owner", f"<@{OWNER}>", True])
+    embed=helpers.create_embed(
+        "Info about the Bot",
+        discord.Colour(0xc387c1),
+        embed_content
     )    
     embed.set_thumbnail(url=f"{bot.user.avatar_url}")
     embed.set_footer(text = datetime.datetime.now())
+    await ctx.send(embed=embed)
+
+@bot.command(name="send")
+async def send(ctx, channel_id, *args):
+    content_list = []
+    for a in args:
+        content_list.append(a+" ")
+    content = ''.join(content_list)
+    channel = bot.get_channel(int(channel_id))
+    await channel.send(content)
+    embed = helpers.create_embed(
+        f"Sent the message!",
+         discord.Colour(0xc387c1),
+          [
+              ["Author", ctx.author.mention, True],
+              ["Channel", channel.mention, True],            
+              ["Content",content,False]
+            ]
+        )
+    await ctx.send(embed=embed)
+
+@bot.command(name="edit")
+async def edit(ctx, channel_id, message_id, *args):
+    channel = bot.get_channel(int(channel_id))
+    msg = await channel.fetch_message(int(message_id))
+    original_content = msg.content    
+    content_list = []
+    for a in args:
+        content_list.append(a+" ")
+    content = ''.join(content_list)
+    await msg.edit(content=content)
+    embed = helpers.create_embed(
+        f"Edited the message!",
+         discord.Colour(0xc387c1),
+          [
+              ["Author", ctx.author.mention, True],
+              ["Channel", channel.mention, True],            
+              ["Original Content",original_content,False],
+              ["New Content", content, False]
+            ]
+        )
     await ctx.send(embed=embed)
 
 #  Returns the bot side latency
