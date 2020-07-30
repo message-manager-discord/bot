74f1e239cb29e6d92815616f11c9cbd27019e6f4
Another Cat <Another Cat@example.com>
2020-07-30 15:51:52 +0800

    change logger from write mode to append mode

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 # Start up the discord logging module.
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
