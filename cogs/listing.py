8827a3a82fc3fba792ea77a63aadfc2245cee4a8
AnotherCat <anothercat1259@gmail.com>
2020-09-30 10:18:49 +1300

    Make stats posting more streamlined

--- cogs/listing.py
+++ cogs/listing.py
@@ -10,46 +10,54 @@ class ListingCog(commands.Cog):
 
     def cog_unload(self):
         self.stats_post_loop.cancel()
+
+    async def post_guild_stats(
+        self,
+        session,
+        url,
+        name_of_param,
+        token,
+        error_name,
+        expected_returned_value):
+        async with session.post(
+            url = url,
+            headers = {
+                'Authorization' : token,
+                'Content-Type' : 'application/json'
+            },
+            json = {
+                name_of_param, len(self.bot.guilds)
+            }
+        ) as r:
+            returned = await r.json()
+            if not returned[error_name] is expected_returned_value:
+                print(f'Error posting to {url}')
+            else:
+                print(f'Posted to {url}')
+
+
     
     @tasks.loop(minutes=30)
     async def stats_post_loop(self):
         await self.bot.wait_until_ready()
         async with aiohttp.ClientSession() as session:
-            async with session.post(
-                url = f'https://discordbotlist.com/api/v1/bots/{self.bot.user.id}/stats',
-                headers = {
-                    'Authorization' : self.bot.dbl_token,
-                    'Content-Type' : 'application/json'
-                },
-                json = {
-                    'guilds' : len(self.bot.guilds)
-                }
-            ) as r:
-                returned = await r.json()
-                if not returned['success']:
-                    print('Posting to discordbotlist.com failed!')
-                else:
-                    print('Posting to discordbotlist.com successful!')
+            await self.post_guild_stats(
+                session,
+                f'https://discordbotlist.com/api/v1/bots/{self.bot.user.id}/stats',
+                'guilds',
+                self.bot.dbl_token,
+                'success',
+                True
+            )
+            await self.post_guild_stats(
+                session,
+                f'https://discord.boats/api/bot/{self.bot.user.id}',
+                'server_count',
+                self.bots.dboats_token,
+                'error',
+                False
+            )
         
-    
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                url = f'https://discord.boats/api/bot/{self.bot.user.id}',
-                headers = {
-                    'Authorization' : self.bot.dboats_token,
-                    'Content-Type' : 'application/json'
-                },
-                json = {
-                    'server_count' : len(self.bot.guilds)
-                }
-            ) as r:
-                returned = await r.json()
-                if returned['error']:
-                    print('Posting to discord.boats failed!' +returned['message'])
-                else:
-                    print('Posting to discord.boats successful!')
-        
-
 
 def setup(bot):
     bot.add_cog(ListingCog(bot))
