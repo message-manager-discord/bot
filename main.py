a88c6fb3a5b19c12b5ccaa52b188749444911cd1
AnotherCat <anothercat1259@gmail.com>
2020-11-24 19:53:34 +1300

    chore: ignore command missing errors

--- main.py
+++ main.py
@@ -56,6 +56,12 @@ class Bot(commands.Bot):
             prefix = [prefix, ""]
         return commands.when_mentioned_or(*prefix)(self, message)
 
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            pass
+        else:
+            raise error
+
 
 async def run():
     database = db.DatabasePool(config.uri, bot)
