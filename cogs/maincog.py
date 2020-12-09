message-manager-discord/bot

commit 316c9f4bd1bb13cf6db8b2acdafc82ad53adb222

fix: send correct prefix when mention used
stop mention prefix being unformatted
return name instead of mention

cogs/maincog.py
@@ -38,7 +38,11 @@ class MainCog(commands.Cog):
         if cog:
             if cog._get_overridden_method(cog.cog_command_error) is not None:
                 return
-        await ctx.send("There was an unknown error!\n" f"Error: {error}")
+        await ctx.send(
+            "There was an unknown error!\n"
+            f"Report a bug or get support from the support server at {self.bot.command_with_prefix(ctx, 'support')}\n"
+            f"Error: {error}"
+        )
         raise error
 
     @commands.Cog.listener()
@@ -121,7 +125,6 @@ class MainCog(commands.Cog):
     )
     async def help(self, ctx: commands.Context, option: str = None):
         if option is None or option.lower() != "setup":
-            prefix = await self.bot.db.get_prefix(ctx.guild)
             embed = discord.Embed(
                 title="Help!",
                 colour=16761035,
@@ -129,38 +132,42 @@ class MainCog(commands.Cog):
                 url="https://docs.messagemanager.xyz",
             )
             embed.add_field(
-                name=f"`{prefix}ping`",
+                name=self.bot.command_with_prefix(ctx, "ping"),
                 value="Replies with the latency of the bot",
                 inline=True,
             )
             embed.add_field(
-                name=f"`{prefix}help`", value="Displays this view.", inline=True
+                name=self.bot.command_with_prefix(ctx, "help"),
+                value="Displays this view.",
+                inline=True,
             ),
             embed.add_field(
-                name=f"`{prefix}info`", value="Displays info about the bot", inline=True
+                name=self.bot.command_with_prefix(ctx, "info"),
+                value="Displays info about the bot",
+                inline=True,
             ),
             embed.add_field(
-                name=f"`{prefix}send [channel_id] [content]`",
+                name=f"{self.bot.command_with_prefix(ctx, 'send')} [channel_id] [content]`",
                 value="Sends a message from the bot in the specificed channel",
                 inline=True,
             )
             embed.add_field(
-                name=f"`{prefix}edit [channel_id] [message_id] [new_content]`",
+                name=f"{self.bot.command_with_prefix(ctx, 'edit')} [channel_id] [message_id] [new_content]`",
                 value="Edits a message, message **must** be from the bot",
                 inline=True,
             )
             embed.add_field(
-                name=f"`{prefix}fetch [channel_id] [message_id]`",
+                name=f"{self.bot.command_with_prefix(ctx, 'fetch')} [channel_id] [message_id]`",
                 value="Returns raw content of the message in a .txt file",
                 inline=True,
             )
             embed.add_field(
-                name=f"`{prefix}delete [channel_id] [message_id]`",
+                name=f"{self.bot.command_with_prefix(ctx, 'delete')} [channel_id] [message_id]`",
                 value="Deletes the message from the bot. **Must** be from the bot",
                 inline=True,
             )
             embed.add_field(
-                name=f"`{prefix}setup`",
+                name=self.bot.command_with_prefix(ctx, "setup"),
                 value="Starts setup, Requires the user to have admin permissions",
                 inline=True,
             )

cogs/setup.py
@@ -43,14 +43,17 @@ class SetupCog(commands.Cog):
             await ctx.send(error)
 
         else:
-            await ctx.send("There was an unknown error!\n" f"Error: {error}")
+            await ctx.send(
+                "There was an unknown error!\n"
+                f"Report a bug or get support from the support server at {self.bot.command_with_prefix(ctx, 'support')}\n"
+                f"Error: {error}"
+            )
             raise error
 
     @commands.has_guild_permissions(administrator=True)
     @commands.group()
     async def setup(self, ctx: commands.Context):
         if ctx.invoked_subcommand is None:
-            prefix = await self.bot.db.get_prefix(ctx.guild)
             embed = discord.Embed(
                 title="Setup!",
                 description="Setup values for your server!",
@@ -58,22 +61,25 @@ class SetupCog(commands.Cog):
                 colour=discord.Colour(15653155),
             )
             embed.add_field(
-                name=f"`{prefix}setup admin " "{role}`",
+                name=f"{self.bot.command_with_prefix(ctx, 'setup')} admin " "{role}`",
                 value="This is the role that allows admin access to admin commands.",
                 inline=False,
             )
             embed.add_field(
-                name=f"`{prefix}setup prefix " "{prefix}`",
+                name=f"{self.bot.command_with_prefix(ctx, 'setup')} prefix "
+                "{prefix}`",
                 value="Sets the prefix for this server.",
                 inline=False,
             )
             embed.add_field(
-                name=f"`{prefix}setup botstats " "{channel}`",
+                name=f"{self.bot.command_with_prefix(ctx, 'setup')} botstats "
+                "{channel}`",
                 value="This feature has been disabled! See [changelog](https://github.com/AnotherCat/message-bot/blob/master/CHANGELOG.md/#v110)",
                 inline=False,
             )
             embed.add_field(
-                name=f"`{prefix}setup userstats " "{channel}`",
+                name=f"{self.bot.command_with_prefix(ctx, 'setup')} userstats "
+                "{channel}`",
                 value="This feature has been disabled! See [changelog](https://github.com/AnotherCat/message-bot/blob/master/CHANGELOG.md/#v110)",
                 inline=False,
             )

cogs/admin.py
@@ -52,7 +52,11 @@ class AdminCog(commands.Cog):
         ):
             await ctx.send(error)
         else:
-            await ctx.send("There was an unknown error!\n" f"Error: {error}")
+            await ctx.send(
+                "There was an unknown error!\n"
+                f"Report a bug or get support from the support server at {self.bot.command_with_prefix(ctx, 'support')}\n"
+                f"Error: {error}"
+            )
             raise error
 
     @commands.command(hidden=True)

cogs/messages.py
@@ -60,7 +60,11 @@ class MessagesCog(commands.Cog):
                 "Run the command without pararameters to be guided through the input!"
             )
         else:
-            await ctx.send("There was an unknown error!\n" f"Error: {error}")
+            await ctx.send(
+                "There was an unknown error!\n"
+                f"Report a bug or get support from the support server at {self.bot.command_with_prefix(ctx, 'support')}\n"
+                f"Error: {error}"
+            )
             raise error
 
     async def check_channel(self, ctx: commands.Context, channel):
@@ -435,7 +439,7 @@ class MessagesCog(commands.Cog):
         except json.decoder.JSONDecodeError as e:
             raise self.bot.errors.JSONFailure(
                 "The json that you specified was not correct, please check and try again.\n"
-                f"Get support from the docs or the support server at `{ctx.prefix}support`\n"
+                f"Get support from the docs or the support server at {self.bot.command_with_prefix(ctx, 'support')}\n"
                 f"Error message: {e}"
             )
 
@@ -488,7 +492,7 @@ class MessagesCog(commands.Cog):
             raise self.bot.errors.DifferentAuthor()
         if len(msg.embeds) == 0:
             raise self.bot.errors.InputContentIncorrect(
-                f"That message does not have an embed! Try `{ctx.prefix}edit` instead"
+                f"That message does not have an embed! Try {self.bot.command_with_prefix(ctx, 'edit')} instead"
             )
         elif len(msg.embeds) > 1:
             raise self.bot.errors.InputContentINcorrect(
@@ -558,7 +562,7 @@ class MessagesCog(commands.Cog):
             raise self.bot.errors.DifferentAuthor()
         if len(msg.embeds) == 0:
             raise self.bot.errors.InputContentIncorrect(
-                f"That message does not have an embed! Try `{ctx.prefix}edit` instead"
+                f"That message does not have an embed! Try {self.bot.command_with_prefix(ctx, 'edit')} instead"
             )
         elif len(msg.embeds) > 1:
             raise self.bot.errors.InputContentINcorrect(
@@ -579,7 +583,7 @@ class MessagesCog(commands.Cog):
         except json.decoder.JSONDecodeError as e:
             raise self.bot.errors.JSONFailure(
                 "The json that you specified was not correct, please check and try again.\n"
-                f"Get support from the docs or the support server at `{ctx.prefix}support`\n"
+                f"Get support from the docs or the support server at {self.bot.command_with_prefix(ctx, 'support')}\n"
                 f"Error message: {e}"
             )
 

cogs/stats.py
@@ -37,7 +37,11 @@ class StatsCog(commands.Cog):
         ):
             await ctx.send(error)
         else:
-            await ctx.send("There was an unknown error!\n" f"Error: {error}")
+            await ctx.send(
+                "There was an unknown error!\n"
+                f"Report a bug or get support from the support server at {self.bot.command_with_prefix(ctx, 'support')}\n"
+                f"Error: {error}"
+            )
             raise error
 
     @commands.group()

main.py
@@ -56,6 +56,12 @@ class Bot(commands.Bot):
             prefix = [prefix, ""]
         return commands.when_mentioned_or(*prefix)(self, message)
 
+    def command_with_prefix(self, ctx, command_name):
+        if str(self.user.id) in ctx.prefix:
+            return f"`@{ctx.me.nick or ctx.me.name} {command_name}`"
+        else:
+            return f"`{ctx.prefix}{command_name}`"
+
 
 async def run():
     database = db.DatabasePool(config.uri, bot)

commit 92cfc12928b1c108d4034ff6e62941cac037c9d4

fix: incorrect formatting
bug caused by previous commit 316c9f4bd1bb13cf6db8b2acdafc82ad53adb222

cogs/maincog.py
@@ -147,22 +147,28 @@ class MainCog(commands.Cog):
                 inline=True,
             ),
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'send')} [channel_id] [content]`",
+                name=self.bot.command_with_prefix(ctx, "send [channel_id] [content]"),
                 value="Sends a message from the bot in the specificed channel",
                 inline=True,
             )
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'edit')} [channel_id] [message_id] [new_content]`",
+                name=self.bot.command_with_prefix(
+                    ctx, "edit [channel_id] [message_id] [new_content]"
+                ),
                 value="Edits a message, message **must** be from the bot",
                 inline=True,
             )
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'fetch')} [channel_id] [message_id]`",
+                name=self.bot.command_with_prefix(
+                    ctx, "fetch [channel_id] [message_id]"
+                ),
                 value="Returns raw content of the message in a .txt file",
                 inline=True,
             )
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'delete')} [channel_id] [message_id]`",
+                name=self.bot.command_with_prefix(
+                    ctx, "delete [channel_id] [message_id]"
+                ),
                 value="Deletes the message from the bot. **Must** be from the bot",
                 inline=True,
             )

cogs/setup.py
@@ -61,25 +61,22 @@ class SetupCog(commands.Cog):
                 colour=discord.Colour(15653155),
             )
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'setup')} admin " "{role}`",
+                name=self.bot.command_with_prefix(ctx, "setup admin {role}"),
                 value="This is the role that allows admin access to admin commands.",
                 inline=False,
             )
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'setup')} prefix "
-                "{prefix}`",
+                name=self.bot.command_with_prefix(ctx, "setup prefix {prefix}"),
                 value="Sets the prefix for this server.",
                 inline=False,
             )
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'setup')} botstats "
-                "{channel}`",
+                name=self.bot.command_with_prefix(ctx, "setup botstats {channel}"),
                 value="This feature has been disabled! See [changelog](https://github.com/AnotherCat/message-bot/blob/master/CHANGELOG.md/#v110)",
                 inline=False,
             )
             embed.add_field(
-                name=f"{self.bot.command_with_prefix(ctx, 'setup')} userstats "
-                "{channel}`",
+                name=self.bot.command_with_prefix(ctx, "setup userstats {channel}"),
                 value="This feature has been disabled! See [changelog](https://github.com/AnotherCat/message-bot/blob/master/CHANGELOG.md/#v110)",
                 inline=False,
             )