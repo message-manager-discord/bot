26b14fcf3c9d929213087760d202b337f89d39e4
AnotherCat <anothercat1259@gmail.com>
2021-01-08 20:41:04 +1300

    feat: add base description for /info cmd

--- cogs/slash_cmds.py
+++ cogs/slash_cmds.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING
 
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext, cog_ext
@@ -12,7 +12,7 @@ else:
     Cog = commands.Cog
 
 
-guilds: List[int] = []
+info_base_description = "Information Commands"
 
 
 class Slash(Cog):
@@ -23,56 +23,72 @@ class Slash(Cog):
                 bot, override_type=True, auto_register=True, auto_delete=True
             )
         self.bot = bot
-        guilds = self.bot.slash_guilds  # noqa F841
         self.bot.slash.get_cog_commands(self)
 
     def cog_unload(self) -> None:
         self.bot.slash.remove_cog_commands(self)
 
-    """ Add this command again once a set of testing commands is added
-    @cog_ext.cog_slash(name="beta-slash", description="How to enable slash commands for this bot!")
-    async def _beta_slash(self, ctx: SlashContext) -> None:
-        message = (
-            "Slash commands are here!"
-            "\nCurrently the slash commands on this bot are in a beta testing phase"
-            "\nJoin the [support server](https://discord.gg/xFZu29t) "
-            "for instructions on how to join the beta (if you have not already)"
-        )
-        await ctx.send(content=message, complete_hidden=True)
-    """
-
-    @cog_ext.cog_subcommand(base="info", name="info", description="Bot information")
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="info",
+        description="Bot information",
+        base_description=info_base_description,
+    )
     async def _info(self, ctx: SlashContext) -> None:
         embed = await shared_commands.create_info_embed(ctx, self.bot)
         await ctx.send(embeds=[embed])
 
     @cog_ext.cog_subcommand(
-        base="info", name="privacy", description="Privacy information"
+        base="info",
+        name="privacy",
+        description="Privacy information",
+        base_description=info_base_description,
     )
     async def privacy(self, ctx: SlashContext) -> None:
         embed = shared_commands.create_privacy_embed()
         await ctx.send(embeds=[embed])
 
     @cog_ext.cog_subcommand(
-        base="info", name="ping", description="Returns the current gateway latency"
+        base="info",
+        name="ping",
+        description="Returns the current gateway latency",
+        base_description=info_base_description,
     )
     async def _ping(self, ctx: SlashContext) -> None:
         await ctx.send(content=f"Gateway latency: {round(self.bot.latency*1000, 2)}ms")
 
-    @cog_ext.cog_subcommand(base="info", name="invite", description="Bot invite")
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="invite",
+        description="Bot invite",
+        base_description=info_base_description,
+    )
     async def _invite(self, ctx: SlashContext) -> None:
         await ctx.send(embeds=[shared_commands.create_invite_embed()])
 
-    @cog_ext.cog_subcommand(base="info", name="docs", description="Bot documentation")
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="docs",
+        description="Bot documentation",
+        base_description=info_base_description,
+    )
     async def _docs(self, ctx: SlashContext) -> None:
         await ctx.send(embeds=[shared_commands.create_docs_embed()])
 
-    @cog_ext.cog_subcommand(base="info", name="source", description="Bot's source code")
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="source",
+        description="Bot's source code",
+        base_description=info_base_description,
+    )
     async def _source(self, ctx: SlashContext) -> None:
         await ctx.send(embeds=[shared_commands.create_source_embed()])
 
     @cog_ext.cog_subcommand(
-        base="info", name="support", description="Join my support server!"
+        base="info",
+        name="support",
+        description="Join my support server!",
+        base_description=info_base_description,
     )
     async def _support(self, ctx: SlashContext) -> None:
         await ctx.send(embeds=[shared_commands.create_support_embed()])
