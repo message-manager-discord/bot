00b83a204c569849450bae26da9c996eb7ad83a9
AnotherCat <anothercat1259@gmail.com>
2021-01-11 01:04:46 +1300

    chore: combine slash & normal cmds to single cogs

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -17,15 +17,16 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+import platform
 
 from datetime import datetime, timezone
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Union
 
 import discord
 
 from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext, cog_ext
 
-from cogs.utils import shared_commands
 from main import Bot
 
 if TYPE_CHECKING:
@@ -33,10 +34,136 @@ if TYPE_CHECKING:
 else:
     Cog = commands.Cog
 
+info_base_description = "Information Commands"
+
+
+async def create_info_embed(
+    ctx: Union[commands.Context, SlashContext], bot: Bot
+) -> discord.Embed:
+    total_seconds = (datetime.utcnow() - bot.start_time).total_seconds()
+    days = total_seconds // 86400
+    hours = (total_seconds - (days * 86400)) // 3600
+    minutes = (total_seconds - (days * 86400) - (hours * 3600)) // 60
+    seconds = total_seconds - (days * 86400) - (hours * 3600) - (minutes * 60)
+    embed = discord.Embed(
+        title="Info about the bot",
+        colour=discord.Colour(0xC387C1),
+        timestamp=datetime.now(timezone.utc),
+        url="https://messagemanager.xyz",
+    )
+    embed.add_field(name="Username", value=str(bot.user), inline=True),
+    if isinstance(ctx.guild, discord.Guild):
+        in_guild = True
+    else:
+        for guild in bot.guilds:
+            if ctx.guild == guild:
+                in_guild = True
+                break
+        else:
+            in_guild = False
+    if in_guild:
+        prefix = await bot.db.get_prefix(ctx.guild)
+        embed.add_field(name="Prefix", value=f"`{prefix}`", inline=True),
+
+    embed.add_field(name="Version", value=bot.version, inline=True),
+    embed.add_field(
+        name="Docs",
+        value="[The Docs](https://docs.messagemanager.xyz)",
+        inline=True,
+    ),
+    embed.add_field(
+        name="Support Invite",
+        value="[Support Server](https://discord.gg/xFZu29t)",
+        inline=True,
+    )
+    embed.add_field(
+        name="Developer",
+        value="[Another Cat](https://github.com/AnotherCat)",
+        inline=True,
+    ),  # The developer (me), Must not be changed, as per the LICENSE
+    embed.add_field(name="Discord.py Version", value=discord.__version__, inline=True),
+    embed.add_field(
+        name="Python Version", value=platform.python_version(), inline=True
+    ),
+
+    embed.add_field(
+        name="Uptime",
+        value=f"{int(days)} days {int(hours)} hours {int(minutes)} minutes {int(seconds)} seconds",
+        inline=True,
+    ),
+    embed.add_field(name="System", value=platform.system(), inline=True),
+    embed.add_field(name="Number of Servers", value=str(len(bot.guilds)), inline=True)
+    embed.set_thumbnail(url=f"{bot.user.avatar_url}")
+    return embed
+
+
+def create_privacy_embed() -> discord.Embed:
+    embed = discord.Embed(
+        title="Privacy Policy",
+        description="We do store data. Please read our privacy policy.",
+        url="https://github.com/AnotherCat/message-bot/blob/master/PRIVACY_POLICY.md",
+        colour=discord.Colour.red(),
+        timestamp=datetime.now(timezone.utc),
+    )
+    embed.add_field(
+        name="Where to find the privacy policy",
+        value="My privacy policy is located on [github](https://github.com/AnotherCat/message-bot/blob/master/PRIVACY_POLICY.md)",
+    )
+    return embed
+
+
+def create_invite_embed() -> discord.Embed:
+    return discord.Embed(
+        title="Invite me to your server!",
+        description="[Click here](https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=537250880&scope=bot%20applications.commands) to invite me!",
+        url="https://discord.com/api/oauth2/authorize?client_id=735395698278924359&permissions=537250880&scope=bot%20applications.commands",
+        colour=discord.Colour(0xC387C1),
+        timestamp=datetime.now(timezone.utc),
+    )
+
+
+def create_docs_embed() -> discord.Embed:
+    return discord.Embed(
+        title="Docs!",
+        description="My docs are [here](https://docs.messagemanager.xyz)",
+        url="https://docs.messagemanager.xyz",
+        colour=discord.Colour(0xC387C1),
+        timestamp=datetime.now(timezone.utc),
+    )
+
+
+def create_source_embed() -> discord.Embed:
+    return discord.Embed(
+        title="Source Code!",
+        description="My [source code](https://github.com/AnotherCat/message-bot)",
+        url="https://github.com/AnotherCat/message-bot",
+        colour=discord.Colour(0xC387C1),
+        timestamp=datetime.now(timezone.utc),
+    )
+
+
+def create_support_embed() -> discord.Embed:
+    return discord.Embed(
+        title="Join my support server for support!",
+        description="Click [here](https://discord.gg/xFZu29t) to join!",
+        url="https://discord.gg/xFZu29t",
+        colour=discord.Colour(0xC387C1),
+        timestamp=datetime.now(timezone.utc),
+    )
+
 
 class MainCog(Cog):
     def __init__(self, bot: Bot) -> None:
+        if not hasattr(bot, "slash"):
+            # Creates new SlashCommand instance to bot if bot doesn't have.
+            bot.slash = SlashCommand(
+                bot, override_type=True, auto_register=True, auto_delete=True
+            )
         self.bot = bot
+        self.bot.slash.get_cog_commands(self)
+
+    def cog_unload(self) -> None:
+        self.bot.slash.remove_cog_commands(self)
 
     async def on_command_error(
         self, ctx: commands.Context, error: discord.DiscordException
@@ -207,33 +334,110 @@ class MainCog(Cog):
     # Create the info command.
     @commands.command(name="info")
     async def info(self, ctx: commands.Context) -> None:
-        embed = await shared_commands.create_info_embed(ctx, self.bot)
+        embed = await create_info_embed(ctx, self.bot)
         await ctx.send(embed=embed)
+
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="info",
+        description="Bot information",
+        base_description=info_base_description,
+    )
+    async def _info(self, ctx: SlashContext) -> None:
+        embed = await create_info_embed(ctx, self.bot)
+        await ctx.send(embeds=[embed])
+
+    # Ping commands
 
     @commands.command(name="ping")
     async def ping(self, ctx: commands.Context) -> None:
         await ctx.send(f"Gateway latency: {round(self.bot.latency*1000, 2)}ms")
 
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="ping",
+        description="Returns the current gateway latency",
+        base_description=info_base_description,
+    )
+    async def _ping(self, ctx: SlashContext) -> None:
+        await ctx.send(content=f"Gateway latency: {round(self.bot.latency*1000, 2)}ms")
+
+    # Privacy commands
+
     @commands.command()
     async def privacy(self, ctx: commands.Context) -> None:
-        embed = shared_commands.create_privacy_embed()
+        embed = create_privacy_embed()
         await ctx.send(embed=embed)
+
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="privacy",
+        description="Privacy information",
+        base_description=info_base_description,
+    )
+    async def _privacy(self, ctx: SlashContext) -> None:
+        embed = create_privacy_embed()
+        await ctx.send(embeds=[embed])
+
+    # Invite Commands
 
     @commands.command()
     async def invite(self, ctx: commands.Context) -> None:
-        await ctx.send(embed=shared_commands.create_invite_embed())
+        await ctx.send(embed=create_invite_embed())
+
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="invite",
+        description="Bot invite",
+        base_description=info_base_description,
+    )
+    async def _invite(self, ctx: SlashContext) -> None:
+        await ctx.send(embeds=[create_invite_embed()])
+
+    # Docs commands
 
     @commands.command()
     async def docs(self, ctx: commands.Context) -> None:
-        await ctx.send(embed=shared_commands.create_docs_embed())
+        await ctx.send(embed=create_docs_embed())
+
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="docs",
+        description="Bot documentation",
+        base_description=info_base_description,
+    )
+    async def _docs(self, ctx: SlashContext) -> None:
+        await ctx.send(embeds=[create_docs_embed()])
+
+    # Source Commands
 
     @commands.command()
     async def source(self, ctx: commands.Context) -> None:
-        await ctx.send(embed=shared_commands.create_source_embed())
+        await ctx.send(embed=create_source_embed())
+
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="source",
+        description="Bot's source code",
+        base_description=info_base_description,
+    )
+    async def _source(self, ctx: SlashContext) -> None:
+        await ctx.send(embeds=[create_source_embed()])
+
+    # Support Commands
 
     @commands.command()
     async def support(self, ctx: commands.Context) -> None:
-        await ctx.send(embed=shared_commands.create_support_embed())
+        await ctx.send(embed=create_support_embed())
+
+    @cog_ext.cog_subcommand(
+        base="info",
+        name="support",
+        description="Join my support server!",
+        base_description=info_base_description,
+    )
+    async def _support(self, ctx: SlashContext) -> None:
+        await ctx.send(embeds=[create_support_embed()])
 
 
 def setup(bot: Bot) -> None:
